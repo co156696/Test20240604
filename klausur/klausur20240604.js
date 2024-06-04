15baac65f32fbb5063f87bd6d8b1657e86001df4
Oguzhan Cebeci <156648312+co156696@users.noreply.github.com>
2024-06-04 08:54:32 +0000

    ...

--- klausur/klausur20240604.js
+++ klausur/klausur20240604.js
@@ -6,7 +6,8 @@ console.log("Klausur 20240604")
 
 console.log("Aufgabe 1")
 // 1a) Berechnen Sie das Volumen eines Würfels mit Hilfe der Grundrechenoperatoren (+ , - , * , /) , bei dem die Kantenlänge mit der Varablen a festgelegt wird.
-
+let a = 1
+console.log("das volumen ist"+ (a*a*a));
 
 
 // 1b) - Nur Klausurschreiber
@@ -20,11 +21,13 @@ console.log("Aufgabe 2")
 // Umfang = 2 * pi * Radius
 // Durchmesser = 2 * Radius
 
+let Radius= 2
+ 
 
 
 // 2a)
 // Testschreiber deklarieren pi und initialisieren pi mit dem Wert 3.14.
-
+let pi = 3.14
 
 
 // Klausurschreiber deklarieren pi und initialisieren pi mit dem Wert aus der bekannten Bibliothek namens Math. Geben Sie Math.PI an, um den Wert von Pi in einer Variablen speichern zu können.
@@ -34,21 +37,23 @@ console.log("Aufgabe 2")
 
 // 2b)
 // Berechnen Sie Flächeninhalt.
-
+let Flächeninhalt = 3.14 * 2 * 2 
+console.log("der Flächeninhalt ist"+ (3.14*2*2))
 
 
 
 
 // 2c)
 // Berechnen Sie den Umfang
-
-
+let umfang = 2*3.14*2
+console.log("der Umfang ist "+ (2*3.14*2))
 
 
 
 // 2d)
 // Berechnen Sie den Durchmesser
-
+let Durchmesser = 2*2
+ console.log("der Durchmesser ist " + (2*2))
 
 
 
@@ -67,17 +72,23 @@ console.log("Aufgabe 3")
 
 // 3a)
 // Berechnen Sie den Rabatt in einer sprechenden Variablen. Geben Sie einen Anwortsatz auf der Konsole aus.
-
+let Rabatt =  16780*0.05/1
+let listenpreis= 16780
+let RabattProzentsatz= 0.05
+console.log(" der Rabatt beträgt " + (16780*0.05/1))
 
 
 // 3b)
 // Berechnen Sie den Netto-Rechnungsbetrag in einer sprechenden Variablen. Geben Sie einen Anwortsatz auf der Konsole aus.
-
+let NettoRechnungsbetrag = 16780- Rabatt
+console.log(" der NettoRechnungsbetrag beträgt " + (16780- Rabatt))
 
 
 // 3c)
 // Berechnen Sie die MwSt in einer sprechenden Variablen. Geben Sie einen Anwortsatz auf der Konsole aus.
-
+let MwSt = NettoRechnungsbetrag*0.19/1
+let MwStProzentsatz = 0.19
+console.log(" Die MwSt beträgt " + (NettoRechnungsbetrag*0.19/1))
 
 
 
@@ -108,8 +119,10 @@ console.log("Aufgabe 4")
 
 // 4a)
 // Ein Kunde nimmst bei einer Bank einen Kredit über 5000 € auf. Die Laufzeit beträgt 6 Monate bei einem Zinssatz von 10%. Berechne die Zinszahlungen, die in diesem Zeitraum anfallen. Arbeiten Sie mit Variablen. Antwortsatz!
-
-
+k = 5000
+t = 180
+p = 0.1
+console.log(" der zinsbetrag beträgt" + (5000*(0.1/100)* (180/360)))
 
 
 
@@ -132,18 +145,21 @@ console.log("Aufgabe 5")
 // 5a)
 // Erstellen Sie die notwendigen Variablen und weisen Sie beispielhaft Werte zu. 
 // Erstellen Sie zusätzlich eine Variable für den Namen der Person.
-
+let BMI = 68/ (1.7*1.7)
+let körpermasse = 68
+let Körperlänge = 1.70
+console.log(" berechne den BMI von Pit");
 
 
 
 // 5b)
 // Führen Sie die Berechnung durch.
-
+console.log(" Pit sein BMI beträgt " + (68/(1.7*1.7)));
 
 
 // 5c) 
 // Geben Sie den Antwortsatz nach folgendem Muster aus. Die Werte werden aus den Variablen und der vorherigen Berechnung erzeugt: "Pit hat bei einer Körpergröße von 170 cm und einem Körpergewicht von 68 kg einen BMI von 24."
-
+console.log("Pit hat bei einer Größe von 1.7m und einem körpergewicht von 68KG einen BMI von 24" )
 
 
 
